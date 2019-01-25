be9a9c67384316ec30ed9bbe58f993320d922c08
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-01-25 15:30:15 +0000

    build based on f4c01ef

--- dev/search_index.js
+++ dev/search_index.js
@@ -9,27 +9,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "#DependenciesParser.alldeps",
+    "location": "#DependenciesParser.installable",
     "page": "DependenciesParser.jl",
-    "title": "DependenciesParser.alldeps",
+    "title": "DependenciesParser.installable",
     "category": "function",
-    "text": "alldeps(julia::VersionNumber = VersionNumber(1))::Vector{NameRepoVerDeps}\n\nReturns information for all installable packages for the selected julia version\n\n\n\n\n\n"
-},
-
-{
-    "location": "#DependenciesParser.dependencies",
-    "page": "DependenciesParser.jl",
-    "title": "DependenciesParser.dependencies",
-    "category": "function",
-    "text": "dependencies(name::AbstractString,\n             direct::Bool = false,\n             data::VersionNumber = DependenciesParser.data)::Vector{String}\n\nReturns all dependencies for the package (excludes stdlib)\nWhen direct is true, only direct dependencies are returned\nDependencies.data = alldeps()\n\n\n\n\n\n"
-},
-
-{
-    "location": "#DependenciesParser.NameRepoVerDeps",
-    "page": "DependenciesParser.jl",
-    "title": "DependenciesParser.NameRepoVerDeps",
-    "category": "type",
-    "text": "NameRepoVerDeps\n\nAlias for NamedTuple struct used for name, repository, version, and dependencies.\n\n\n\n\n\n"
+    "text": "installable(pkg::AbstractString,\n            julia::VersionNumber = VERSION;\n            direct::Bool = false)::Tuple{Bool,Vector{String}}\n\nReturn whether the package is installable and the dependencies for the solved version.\nIf direct, only direct dependencies are returned.\n\n\n\n\n\n"
 },
 
 {
@@ -37,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "DependenciesParser.jl",
     "title": "DependenciesParser.jl",
     "category": "section",
-    "text": "DependenciesParser.jl is a package for quickly obtaining the list of dependencies for packages.alldeps\ndependencies\nNameRepoVerDeps"
+    "text": "DependenciesParser.jl is a package for quickly obtaining the list of dependencies for packages.installable"
 },
 
 ]}
