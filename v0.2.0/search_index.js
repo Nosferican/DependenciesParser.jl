cf2a15fee5fdadf7d95e0bd7384817e291fd4726
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-01-25 15:32:42 +0000

    build based on f4c01ef

---
+++ v0.2.0/search_index.js
@@ -0,0 +1,27 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "DependenciesParser.jl",
+    "title": "DependenciesParser.jl",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#DependenciesParser.installable",
+    "page": "DependenciesParser.jl",
+    "title": "DependenciesParser.installable",
+    "category": "function",
+    "text": "installable(pkg::AbstractString,\n            julia::VersionNumber = VERSION;\n            direct::Bool = false)::Tuple{Bool,Vector{String}}\n\nReturn whether the package is installable and the dependencies for the solved version.\nIf direct, only direct dependencies are returned.\n\n\n\n\n\n"
+},
+
+{
+    "location": "#DependenciesParser.jl-1",
+    "page": "DependenciesParser.jl",
+    "title": "DependenciesParser.jl",
+    "category": "section",
+    "text": "DependenciesParser.jl is a package for quickly obtaining the list of dependencies for packages.installable"
+},
+
+]}
